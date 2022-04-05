f6e3cb1601c05d365f527492c715eb544fe1da76
Ennio-M <marrese.ennio@gmail.com>
2022-04-05 17:24:39 +0200

    esercizio completato

--- javascript/script.js
+++ javascript/script.js
@@ -1,14 +1,39 @@
+// Inizializzazione variabili elementi
 const app = document.getElementById("app");
 const play = document.getElementById("play");
 
-play.addEventListener("click", generateGrid);
+// Invocazione funzione creazione griglia
+play.addEventListener("click", createGrid);
 
-function generateGrid() {
-    const diff = document.getElementById("diff").value;
-    let cols = "";
+// Funzione creazione griglia
+function createGrid() {
+    const diff = parseInt(document.getElementById("diff").value);
+    let numBox = Math.sqrt(diff);
+    app.innerHTML = "";
     for(let i = 1; i <= diff; i++) {
-        cols += `
-        <div class="col col${diff}">${i}</div>`
+        const box = createBox(i, numBox);
+        app.append(box);
     }
-    app.innerHTML = cols;
+}
+
+// Funzione creazione celle
+function createBox(i, numBox) {
+    let box = document.createElement("div");
+    box.setAttribute("class", "box");
+    box.style.width = `calc((100% - ${numBox}*2px) / ${numBox})`;
+    box.style.height = `calc((100% - ${numBox}*2px) / ${numBox})`;
+    let boxCSS = window.getComputedStyle(app, null);
+    let boxHeight = parseInt(boxCSS.getPropertyValue("height"));
+    box.style.lineHeight = (boxHeight / numBox) + "px";
+    box.classList.add("pointer");
+    box.innerHTML = `<span>${i}</span>`;
+    box.addEventListener("click", clickBox);
+    return box;
+}
+
+//Funzione celle cliccabili
+function clickBox() {
+    this.style.backgroundColor = "#6495ED";
+    this.classList.remove("pointer");
+    this.removeEventListener("click", clickBox);
 }
